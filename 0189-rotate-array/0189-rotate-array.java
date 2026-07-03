10482e3008017c076974a988c3d82b3ea909b55b
BADAL SINGH <badalboss47@gmail.com>
2026-07-03 09:17:32 +0530

    Time: 0 ms (100%), Space: 61 MB (84.25%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+
+        int n = nums.length;
+        k = k % n;
+
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+        // reverse(nums, 0, k);
+        // reverse(nums, k + 1, n - 1);
+        // reverse(nums, 0, n - 1);
+    }
+
+    public void reverse(int[] nums, int i, int j) {
+        // i = 0;
+        // j = nums.length - 1;
+
+        while (i < j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+
+            i++;
+            j--;
+        }
+    }
+}
