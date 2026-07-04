1c1391ece38dbca5222cdae853a462cac683ae25
BADAL SINGH <badalboss47@gmail.com>
2026-07-04 08:59:46 +0530

    Time: 1 ms (99.86%), Space: 45.3 MB (65.4%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int compress(char[] chars) {
+
+        int k = 0, count = 1;
+
+        for (int i = 1; i <= chars.length; i++) {
+            if (i < chars.length && chars[i] == chars[i - 1]) {
+                count++;
+            } else {
+                chars[k++] = chars[i - 1];
+                if (count > 1) {
+                    String str = String.valueOf(count);
+                    for (char c : str.toCharArray()) {
+                        chars[k++] = c;
+                    }
+                }
+                count = 1;
+            }
+        }
+        return k;
+    }
+}
