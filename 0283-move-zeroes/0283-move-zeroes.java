2699f1efb0d62b1df6dc6ec748b0cffec9ace50b
BADAL SINGH <badalboss47@gmail.com>
2026-07-07 07:53:05 +0530

    Time: 2 ms (92.15%), Space: 47.5 MB (93.3%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,31 +1,15 @@
 class Solution {
     public void moveZeroes(int[] nums) {
 
-        /*
         int k = 0;
-        for(int i = 0;i<nums.length;i++){
-        
-            // Step 1: Move non-zero to front
-            if(nums[i] != 0){
-                nums[k] = nums[i];
-                k++;
-            }
-        }
-        
-        // step 2: Fill rest with Zero
-        while(k < nums.length){
-            nums[k++] = 0;
-        }
-        */
-        int j = 0;
+
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-
-                j++;
+                nums[k++] = nums[i];
             }
+        }
+        while (k < nums.length) {
+            nums[k++] = 0;
         }
     }
 }
