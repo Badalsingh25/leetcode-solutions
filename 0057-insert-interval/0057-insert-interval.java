1b8c8acb1b94007bd2446863a251c6d305da30e1
BADAL SINGH <badalboss47@gmail.com>
2026-07-09 07:07:16 +0530

    Time: 1 ms (97.91%), Space: 47 MB (74.9%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -5,23 +5,28 @@ class Solution {
         int n = intervals.length;
         int i = 0;
 
-// Add all elements before newInterval
-        while(i  < n && intervals[i][1] < newInterval[0]){
+        // Add all elements before newInterval
+        while (i < n && intervals[i][1] < newInterval[0]) {
             result.add(intervals[i]);
             i++;
         }
 
-        while(i < n && intervals[i][0] <= newInterval[1]){
+        // Merge every interval that overlaps with newInterval.
+        while (i < n && intervals[i][0] <= newInterval[1]) {
             newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
             newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
             i++;
         }
+
+        // Add the merged interval
         result.add(newInterval);
 
-        while(i < n){
+        // Add all remaining intervals.
+        while (i < n) {
             result.add(intervals[i]);
             i++;
         }
+
         return result.toArray(new int[result.size()][]);
     }
 }
