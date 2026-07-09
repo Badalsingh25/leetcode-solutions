cffe06c6b9e5a918b9bd6f54dbbf781fdb6cd872
BADAL SINGH <badalboss47@gmail.com>
2026-07-09 12:06:08 +0530

    Time: 5 ms (86.66%), Space: 45.4 MB (77.8%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -9,7 +9,6 @@ class Solution {
         for (int right = 0; right < s.length(); right++) {
             if (map.containsKey(s.charAt(right))) {
                 left = Math.max(map.get(s.charAt(right)) + 1, left);
-                map.remove(s.charAt(right));
             }
             map.put(s.charAt(right), right);
             maxLength = Math.max(maxLength, right - left+1 );
