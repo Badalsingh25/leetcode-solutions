3a8c08662629af4337bea22790065dad5b78f414
BADAL SINGH <badalboss47@gmail.com>
2026-07-09 09:56:49 +0530

    Time: 5 ms (61.61%), Space: 44 MB (97.78%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -7,12 +7,11 @@ class Solution {
         int[] count = new int[26];
 
         for (int i = 0; i < s.length(); i++) {
-
             count[s.charAt(i) - 'a']++;
             count[t.charAt(i) - 'a']--;
         }
-        for (int num : count) {
-            if (num != 0) {
+        for (int x : count) {
+            if (x != 0) {
                 return false;
             }
         }
