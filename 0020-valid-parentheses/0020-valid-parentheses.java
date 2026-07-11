340f2eba2999143e00c59c9e91c79f434c64513a
BADAL SINGH <badalboss47@gmail.com>
2026-07-11 06:51:35 +0530

    Time: 4 ms (38.4%), Space: 43.6 MB (10.41%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -7,7 +7,8 @@ class Solution {
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
                 stack.push(s.charAt(i));
-            } else if (s.charAt(i) == ')' || s.charAt(i) == '}' || s.charAt(i) == ']') {
+            } else {
+
                 if (stack.isEmpty()) {
                     return false;
                 }
