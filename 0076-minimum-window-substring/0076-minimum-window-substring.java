b01f89d526e0a513c5245674c15d3572f3c27de2
BADAL SINGH <badalboss47@gmail.com>
2026-07-12 10:34:33 +0530

    Time: 2 ms (99.85%), Space: 45.4 MB (89.26%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -6,9 +6,11 @@ class Solution {
 
         int[] frequency = new int[128];
 
+        // Store frequency of characters in t
         for (char ch : t.toCharArray()) {
             frequency[ch]++;
         }
+
         int left = 0;
         int required = t.length();
         int minLength = Integer.MAX_VALUE;
@@ -17,19 +19,27 @@ class Solution {
         for (int right = 0; right < s.length(); right++) {
 
             char current = s.charAt(right);
+
+            // If this character is still required
             if (frequency[current] > 0) {
                 required--;
             }
+
+            // Include current character in window
             frequency[current]--;
 
+            // Try to shrink the window
             while (required == 0) {
                 if (right - left + 1 < minLength) {
                     minLength = right - left + 1;
                     start = left;
                 }
                 char leftChar = s.charAt(left);
-                frequency[leftChar]++;
 
+                // Remove left character from window
+                frequency[leftChar]++;
+                
+                 // If it becomes required again
                 if (frequency[leftChar] > 0) {
                     required++;
                 }
