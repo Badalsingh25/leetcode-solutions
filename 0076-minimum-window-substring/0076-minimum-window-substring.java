82c333a71967b19cc09ead8126bdbb2fb1522d83
BADAL SINGH <badalboss47@gmail.com>
2026-07-12 09:53:20 +0530

    Time: 2 ms (99.85%), Space: 45.6 MB (76.6%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,41 +1,59 @@
 class Solution {
     public String minWindow(String s, String t) {
 
-        HashMap<Character, Integer> frequency = new HashMap<>();
+        if (s.length() < t.length()) {
+            return "";
+        }
+
+        int[] frequency = new int[128];
+
+        // Store frequency of characters in t
+        for (char ch : t.toCharArray()) {
+            frequency[ch]++;
+        }
+
         int left = 0;
         int required = t.length();
-        int minLength = Integer.MAX_VALUE;
-        String result = "";
 
-        for (char ch : t.toCharArray()) {
-            frequency.put(ch, frequency.getOrDefault(ch, 0) + 1);
-        }
+        int minLength = Integer.MAX_VALUE;
+        int start = 0;
 
         for (int right = 0; right < s.length(); right++) {
 
-            char c = s.charAt(right);
-            if (frequency.containsKey(c)) {
-                if (frequency.get(c) > 0) {
-                    required--;
-                }
-                frequency.put(c, frequency.get(c) - 1);
+            char current = s.charAt(right);
+
+            // If this character is still required
+            if (frequency[current] > 0) {
+                required--;
             }
+
+            // Include current character in window
+            frequency[current]--;
+
+            // Try to shrink the window
             while (required == 0) {
-                int currentLength = right - left + 1;
-                if (currentLength < minLength) {
-                    minLength = currentLength;
-                    result = s.substring(left, right + 1);
+
+                if (right - left + 1 < minLength) {
+                    minLength = right - left + 1;
+                    start = left;
                 }
+
                 char leftChar = s.charAt(left);
-                if (frequency.containsKey(leftChar)) {
-                    frequency.put(leftChar, frequency.get(leftChar) + 1);
-                    if (frequency.get(leftChar) > 0) {
-                        required++;
-                    }
+
+                // Remove left character from window
+                frequency[leftChar]++;
+
+                // If it becomes required again
+                if (frequency[leftChar] > 0) {
+                    required++;
                 }
+
                 left++;
             }
         }
-        return result;
+
+        return minLength == Integer.MAX_VALUE
+                ? ""
+                : s.substring(start, start + minLength);
     }
 }
