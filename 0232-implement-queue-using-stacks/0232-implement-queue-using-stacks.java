545fb9ee9735f3c9b2e62def57e4c6e15edbe082
BADAL SINGH <badalboss47@gmail.com>
2026-07-12 06:12:22 +0530

    Time: 0 ms (100%), Space: 42.8 MB (30.92%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -1,9 +1,11 @@
 class MyQueue {
 
-    Stack<Integer> input = new Stack<>();
-    Stack<Integer> output = new Stack<>();
+    Stack<Integer> input;
+    Stack<Integer> output;
 
     public MyQueue() {
+        input = new Stack<>();
+        output = new Stack<>();
 
     }
 
