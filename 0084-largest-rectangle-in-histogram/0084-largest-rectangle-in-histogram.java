be9e352821d7548bd944eec8b2cf76b9fe83f295
BADAL SINGH <badalboss47@gmail.com>
2026-07-12 08:32:41 +0530

    Time: 58 ms (82.55%), Space: 76.4 MB (81.49%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,37 +1,26 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-
         Stack<Integer> stack = new Stack<>();
-
-        int width = 1;
-        int maxArea = 0;
-        int currentArea = 0;
+        int area = 0;
         int n = heights.length;
-        int[] leftSmaller = new int[n];
-        int[] rightSmaller = new int[n];
 
         for (int i = 0; i < n; i++) {
             while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
-                stack.pop();
+                int bar = stack.pop();
+                int pse = stack.isEmpty() ? -1 : stack.peek();
+                int nse = i;
+
+                area = Math.max(area, heights[bar] * (nse - pse - 1));
             }
-            leftSmaller[i] = stack.isEmpty() ? -1: stack.peek();
             stack.push(i);
         }
 
-        stack.clear();
-
-        for (int i = n - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
-                stack.pop();
-            }
-            rightSmaller[i] = stack.isEmpty() ? n : stack.peek();
-            stack.push(i);
+        while (!stack.isEmpty()) {
+            int bar = stack.pop();
+            int pse = stack.isEmpty() ? -1 : stack.peek();
+            int nse = n;
+            area = Math.max(area, heights[bar] * (nse - pse - 1));
         }
-        for (int i = 0; i < n; i++) {
-            width = rightSmaller[i] - leftSmaller[i] - 1;
-            currentArea = heights[i] * width;
-            maxArea = Math.max(maxArea, currentArea);
-        }
-        return maxArea;
+        return area;
     }
 }
