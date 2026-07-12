ae9f5cf3c8020d1bd852434352fa55437b1bdb95
BADAL SINGH <badalboss47@gmail.com>
2026-07-12 08:37:28 +0530

    Time: 59 ms (79.82%), Space: 76.2 MB (88.68%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -5,16 +5,18 @@ class Solution {
         int n = heights.length;
 
         for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
-                int bar = stack.pop();
-                int pse = stack.isEmpty() ? -1 : stack.peek();
-                int nse = i;
-
+            // Process stack while current bar is smaller than top of stack
+            while (!stack.isEmpty() && heights[i] <= heights[stack.peek()]) {
+                int bar = stack.pop(); // Index of bar
+                int pse = stack.isEmpty() ? -1 : stack.peek(); // Previous smaller element index
+                int nse = i; // current element is next smaller element
+                // width = nse - pse - 1;
                 area = Math.max(area, heights[bar] * (nse - pse - 1));
             }
             stack.push(i);
         }
-
+        
+        // Process remaining bars in stack
         while (!stack.isEmpty()) {
             int bar = stack.pop();
             int pse = stack.isEmpty() ? -1 : stack.peek();
