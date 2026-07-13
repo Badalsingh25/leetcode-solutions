6c988d80a161f46aced9bb9eeac2a48153a77546
BADAL SINGH <badalboss47@gmail.com>
2026-07-13 12:29:30 +0530

    Time: 2 ms (99.28%), Space: 44.2 MB (84.54%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -6,12 +6,13 @@ class Solution {
 
         while (left < right) {
 
-            while (left < right && !Character.isLetterOrDigit(s.charAt(left)))
+            while(left < right && !Character.isLetterOrDigit(s.charAt(left))){
                 left++;
-            while (left < right && !Character.isLetterOrDigit(s.charAt(right)))
+            }
+            while(left < right && !Character.isLetterOrDigit(s.charAt(right))){
                 right--;
-
-            while (left < right && Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+            }
+            while(left < right && Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
                 return false;
             }
             left++;
