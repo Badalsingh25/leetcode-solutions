64db85f3ed783af837beea35202e83a003fc2965
BADAL SINGH <badalboss47@gmail.com>
2026-07-14 16:00:10 +0530

    Time: 9 ms (98.69%), Space: 50.2 MB (57.01%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,40 +1,39 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
 
-        int max = 0;
         int sum = 0;
-
-        for(int num : weights){
-            sum += num;
-            max = Math.max(max, num);
+        int max = 0;
+        for (int val : weights) {
+            sum += val;
+            max = Math.max(max, val);
         }
-        int low = max, high = sum, ans = 0;
-
-        while(low <= high){
+        int low = max;
+        int high = sum;
+        int ans = 0;
+        if (weights.length == days)
+            return max;
+        while (low <= high) {
             int mid = low + (high - low) / 2;
-
-            if(isPossible(weights, mid, days)){
+            if (isPossible(weights, mid, days) == true) {
                 ans = mid;
                 high = mid - 1;
-            }
-            else{
+            } else {
                 low = mid + 1;
             }
         }
         return ans;
     }
-    public boolean isPossible(int[] weights, int capacity, int days){
-        int dayUsed = 1;
-        int sum = 0;
 
-        for(int num : weights){
-            sum += num;
-            if(sum > capacity){
+    public static boolean isPossible(int[] weights, int capacity, int days) {
+        int sum = 0;
+        int dayUsed = 1;
+        for (int i = 0; i < weights.length; i++) {
+            sum += weights[i];
+            if (sum > capacity) {
                 dayUsed++;
-                sum = num;
-                if(dayUsed > days){
+                sum = weights[i];
+                if (dayUsed > days)
                     return false;
-                }
             }
         }
         return dayUsed <= days;
