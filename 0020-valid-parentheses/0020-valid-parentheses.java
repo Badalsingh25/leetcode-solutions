ab14fb2ce0c618f11e80ed7b29090be9a388b637
BADAL SINGH <badalboss47@gmail.com>
2026-07-15 13:49:55 +0530

    Time: 2 ms (98.27%), Space: 43.4 MB (41.6%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,18 +1,22 @@
 class Solution {
     public boolean isValid(String s) {
 
-        Stack<Character> stack = new Stack<>();
+        Stack<Character> stack = new Stack();
 
         for (char ch : s.toCharArray()) {
+
             if (ch == '(')
                 stack.push(')');
             else if (ch == '{')
                 stack.push('}');
             else if (ch == '[')
                 stack.push(']');
+
             else {
-                if (stack.isEmpty() || stack.pop() != ch)
+
+                if (stack.isEmpty() || stack.pop() != ch) {
                     return false;
+                }
             }
         }
         return stack.isEmpty();
