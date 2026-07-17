4cddb62d03a67ee187a7bebb352e25e74d7e573b
BADAL SINGH <badalboss47@gmail.com>
2026-07-17 07:29:15 +0530

    Time: 0 ms (100%), Space: 43.4 MB (15.23%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -17,8 +17,8 @@ class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
 
         List<Integer> ans = new ArrayList<>();
-        if (root == null)
-            return ans;
+        // if (root == null)
+        //     return ans;
 
         preOrder(root, ans);
         return ans;
