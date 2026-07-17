1e00a22eaabbe273376c76422e87e5dd8869eee6
BADAL SINGH <badalboss47@gmail.com>
2026-07-17 11:36:50 +0530

    Time: 0 ms (100%), Space: 45.3 MB (79.31%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -32,28 +32,29 @@ class Solution {
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
 
-        List<List<Integer>> list = new ArrayList<>();
-
-        backtrack(0, nums, list);
-        return list;
+       List<List<Integer>> result = new ArrayList<>();
+        backtrack(nums, 0, result);
+        return result;
     }
-    public void backtrack(int start, int[] nums, List<List<Integer>> result){
 
-        if(start == nums.length){
-            List<Integer> perm  = new ArrayList<>();
-            for(int num : nums){
+    private void backtrack(int[] nums, int start, List<List<Integer>> result) {
+
+        if (start == nums.length) {
+            List<Integer> perm = new ArrayList<>();
+            for (int num : nums) {
                 perm.add(num);
             }
             result.add(perm);
             return;
         }
-        for(int i = start; i < nums.length;i++){
-            swap(nums, start, i);
-            backtrack(start + 1, nums, result);
-            swap(nums, start, i);
+        for (int i = start; i < nums.length; i++) {
+            swap(nums, start, i); // place a new number at position "start" (choose)
+            backtrack(nums, start + 1, result); // Now fix the next position (Explore)
+            swap(nums, start, i); // Restore the Array to it's original state (Undo)
         }
     }
-    private void swap(int[] nums, int i , int j){
+
+    private void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
