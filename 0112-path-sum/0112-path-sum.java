e932af2474fb7e4270b1173c43157e550538a9cd
BADAL SINGH <badalboss47@gmail.com>
2026-07-19 09:54:07 +0530

    Time: 0 ms (100%), Space: 44.8 MB (68.39%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -15,15 +15,14 @@
  */
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        
-        if(root == null) return false;
-        if(root.left == null && root.right == null) {
-           if(targetSum == root.val){
-            return true;
-           } 
+
+        if (root == null)
+            return false;
+        if (root.left == null && root.right == null) {
+            return targetSum == root.val;
         }
-        int remaining = targetSum - root.val; 
-        
-        return hasPathSum(root.left, remaining) || hasPathSum(root.right, remaining); 
+        int remaining = targetSum - root.val;
+
+        return hasPathSum(root.left, remaining) || hasPathSum(root.right, remaining);
     }
 }
