393aa5804e69f8054f996f74dc7706dd1c5aa4e5
BADAL SINGH <badalboss47@gmail.com>
2026-07-21 08:07:00 +0530

    Time: 0 ms (100%), Space: 45.1 MB (65.96%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -14,20 +14,34 @@
  * }
  */
 class Solution {
+
     public boolean isValidBST(TreeNode root) {
 
-        return (validate(root, Long.MIN_VALUE, Long.MAX_VALUE));
+        // Initially, every value is allowed
+        // Range = (-∞, +∞)
+        return validate(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
 
     private boolean validate(TreeNode root, long min, long max) {
+
+        // Empty tree is always a valid BST
         if (root == null)
             return true;
 
-        if (root.val <= min || root.val >= max) {
+        // Current node must lie within the allowed range
+        // BST does NOT allow duplicates
+        if (root.val <= min || root.val >= max)
             return false;
-        }
+
+        // Left subtree:
+        // Values must be smaller than current node
         boolean leftSubtree = validate(root.left, min, root.val);
+
+        // Right subtree:
+        // Values must be greater than current node
         boolean rightSubtree = validate(root.right, root.val, max);
+
+        // BST is valid only if BOTH subtrees are valid
         return leftSubtree && rightSubtree;
     }
 }
