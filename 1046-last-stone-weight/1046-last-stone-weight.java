343986795c5ba06ae37c435adc7148142af335e8
BADAL SINGH <badalboss47@gmail.com>
2026-07-28 07:36:56 +0530

    Time: 2 ms (34.51%), Space: 42.7 MB (70.39%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -6,10 +6,10 @@ class Solution {
         for (int stone : stones) {
             pq.offer(stone);
         }
-        
+
         while (pq.size() > 1) {
-            int first = pq.poll();
-            int second = pq.poll();
+            int first = pq.poll(); // largest element
+            int second = pq.poll(); // second largest element
 
             if (first != second)
                 pq.offer(first - second);
