a482599c128f15191665c411f418a1ed1e10e438
BADAL SINGH <badalboss47@gmail.com>
2026-07-29 11:53:17 +0530

    Time: 30 ms (66.74%), Space: 53.6 MB (78.83%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,12 +1,8 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
 
-        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> {
-            int distA = a[0] * a[0] + a[1] * a[1];
-            int distB = b[0] * b[0] + b[1] * b[1];
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> (b[0] * b[0] + b[1] * b[1]) - (a[0] * a[0] + a[1] * a[1]));
 
-            return distB - distA;
-        });
         for (int[] point : points) {
             maxHeap.offer(point);
 
