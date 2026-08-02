46c4873eed7aeb997a4a81508db5014d0e74df02
BADAL SINGH <badalboss47@gmail.com>
2026-08-02 07:34:11 +0530

    Time: 32 ms (80.55%), Space: 139.2 MB (28.37%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
@@ -1,28 +1,36 @@
 class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
 
-        List<List<Integer>> list = new ArrayList<>();
-        if (nums1.length == 0 || nums2.length == 0 || k == 0)
-            return list;
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>(
-                (a, b) -> (nums1[a[0]] + nums2[a[1]]) - (nums1[b[0]] + nums2[b[1]]));
+        List<List<Integer>> ans = new ArrayList<>();
 
+        if (nums1.length == 0 || nums2.length == 0 || k == 0)
+            return ans;
+
+        // {i, j}
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+            (a, b) -> (nums1[a[0]] + nums2[a[1]]) - (nums1[b[0]] + nums2[b[1]])
+        );
+
+        // Har row ka first element
         for (int i = 0; i < Math.min(nums1.length, k); i++) {
-            minHeap.offer(new int[] { i, 0 });
+            pq.offer(new int[]{i, 0});
         }
 
-        while (k-- > 0 && !minHeap.isEmpty()) {
+        while (k-- > 0 && !pq.isEmpty()) {
 
-            int[] curr = minHeap.poll();
-            int i = curr[0];
-            int j = curr[1];
+            int[] cur = pq.poll();
 
-            list.add(Arrays.asList(nums1[i], nums2[j]));
+            int i = cur[0];
+            int j = cur[1];
 
+            ans.add(Arrays.asList(nums1[i], nums2[j]));
+
+            // Same row ka next element
             if (j + 1 < nums2.length) {
-                minHeap.offer(new int[] { i, j + 1 });
+                pq.offer(new int[]{i, j + 1});
             }
         }
-        return list;
+
+        return ans;
     }
 }
