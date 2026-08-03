46221d4ae738a7e6f9316a2035eb4b2a8dc78d75
BADAL SINGH <badalboss47@gmail.com>
2026-08-03 07:45:49 +0530

    Time: 3 ms (76.73%), Space: 42.9 MB (70.48%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -2,8 +2,8 @@ class Solution {
     public String reorganizeString(String s) {
 
         int[] freq = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            freq[s.charAt(i) - 'a']++;
+        for (char ch : s.toCharArray()) {
+            freq[ch - 'a']++;
         }
 
         PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> b[1] - a[1]);
@@ -18,21 +18,23 @@ class Solution {
         while (!maxHeap.isEmpty()) {
 
             int[] curr = maxHeap.poll();
-            int index = curr[0];
-            int frequency = curr[1];
+            // int index = curr[0];
+            // int frequency = curr[1];
 
-            char ch = (char) (index + 'a');
-            sb.append(ch);
+            // char ch = (char) (index + 'a');
+            // sb.append(ch);
+            sb.append((char) (curr[0] + 'a'));
             curr[1]--;
 
             if (prev != null)
                 maxHeap.offer(prev);
 
-            if (curr[1] > 0)
-                // prev = new int[] { index, curr[1] };
-                prev = curr;
-            else
-                prev = null;
+            // if (curr[1] > 0)
+            //     // prev = new int[] { index, curr[1] };
+            //     prev = curr;
+            // else
+            //     prev = null;
+            prev = (curr[1] > 0) ? curr : null;
         }
         return sb.length() == s.length() ? sb.toString() : "";
     }
