43759685977b40a2437c3870d4e92f962979ba4d
BADAL SINGH <badalboss47@gmail.com>
2026-08-05 07:38:11 +0530

    Time: 0 ms (100%), Space: 43.7 MB (63.78%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -6,16 +6,13 @@ class Solution {
         int k = m + n - 1;
 
         while (i >= 0 && j >= 0) {
-            
+
             if (nums1[i] >= nums2[j]) {
                 nums1[k] = nums1[i--];
             } else {
                 nums1[k] = nums2[j--];
             }
             k--;
-        }
-        while (i >= 0) {
-            nums1[k--] = nums1[i--];
         }
         while (j >= 0) {
             nums1[k--] = nums2[j--];
