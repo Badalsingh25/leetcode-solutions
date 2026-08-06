cb67b29275f245b38fbdbb469d6d154fd290f3a4
BADAL SINGH <badalboss47@gmail.com>
2026-08-06 07:35:58 +0530

    Time: 2 ms (84.37%), Space: 47.8 MB (15.96%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.java
+++ 0941-valid-mountain-array/0941-valid-mountain-array.java
@@ -1,29 +1,17 @@
 class Solution {
     public boolean validMountainArray(int[] arr) {
-/*
-        int n = arr.length, i = 0;
 
-        if (n < 3)
-            return false;
-       
-        while (i + 1 < n && arr[i] < arr[i + 1]) { //climb up
-            i++;
-        }
+        int n = arr.length;
+        if(n < 3) return false;
+    
+        int i = 0;
 
-        //peak cannot be first or last
-        if (i == 0 || i == n - 1)
-            return false;
-        
-        while (i + 1 < n && arr[i] > arr[i + 1]) { //climb down
-            i++;
-        }
+            while(i < n - 1 && arr[i] < arr[i+1]) i++;
+
+            if(i == 0 || i == n - 1) return false;
+
+            while(i < n - 1 && arr[i] > arr[i+1]) i++;
+
         return i == n - 1;
-*/
-
-    if(arr.length < 3) return false;
-        int l = 0;
-        int r = arr.length - 1;
-        while(l + 1 < arr.length - 1 && arr[l] < arr[l + 1]) l++;
-        while(r - 1 > 0 && arr[r] < arr[r - 1]) r--;
-        return l == r;
-}}
+    }
+}
